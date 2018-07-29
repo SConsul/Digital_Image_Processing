bbfc7ce73d4bfa20f3ad48962a33a184cd91d4fd
SConsul <sarthakconsul@gmail.com>
2018-07-29 23:49:25 +0530

    main script update

--- Assignment_1/Q2/codes/myMainScript.m
+++ Assignment_1/Q2/codes/myMainScript.m
@@ -3,4 +3,13 @@
 tic;
 %% Your code here
 
+%Q2(a) Image Enhancement by Linear Contrasting
+church = imread('../data/church.png');
+church_linstretch = myLinearContrastStretching(church);
+imshow(church_linstretch);
+
+%Q2(b) Image Enhancement using Independent channel Histogram Equalisation
+HM = imread('../data/HMInputImage.png');
+HM_he = myHE(HM);
+imshow(HM_he);
 toc;
