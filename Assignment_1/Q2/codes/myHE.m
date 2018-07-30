3370b81f614a542664057882ffdeddea32adf391
SConsul <sarthakconsul@gmail.com>
2018-07-30 18:47:59 +0530

    updated he

--- Assignment_1/Q2/codes/myHE.m
+++ Assignment_1/Q2/codes/myHE.m
@@ -3,22 +3,22 @@ function output = myHE(input)
 %         plot(hist/sum(hist));
 %         hold on;
 %         plot(cdf)
-      r_he = input;
-      g_he = input;
-      b_he = input;
+      r_i = input(:,:,1);
+      g_i = input(:,:,2);
+      b_i = input(:,:,3);
       output = input;
       
-      cdf_r = CDF_gen(input(:,:,1));
-      cdf_g = CDF_gen(input(:,:,2));
-      cdf_b = CDF_gen(input(:,:,3));
+      cdf_r = CDF_gen(r_i);
+      cdf_g = CDF_gen(g_i);
+      cdf_b = CDF_gen(b_i);
      
-      r_he(:,:,1) = arrayfun(@(z)CDF(cdf_r,z),input(:,:,1));
-      g_he(:,:,2) = arrayfun(@(z)CDF(cdf_g,z),input(:,:,2));
-      b_he(:,:,3) = arrayfun(@(z)CDF(cdf_b,z),input(:,:,3));
+      r_he = arrayfun(@(z)CDF(cdf_r,z),r_i);
+      g_he = arrayfun(@(z)CDF(cdf_g,z),g_i);
+      b_he = arrayfun(@(z)CDF(cdf_b,z),b_i);
       
-      output(:,:,1) = r_he(:,:,1);
-      output(:,:,2) = g_he(:,:,2);
-      output(:,:,3) = b_he(:,:,3);
+      output(:,:,1) = r_he;
+      output(:,:,2) = g_he;
+      output(:,:,3) = b_he;
       
 %       subplot(1,2,1),imshow(input);
 %       subplot(1,2,2),imshow(output);
--- Assignment_1/Q2/codes/myMainScript.m
+++ Assignment_1/Q2/codes/myMainScript.m
@@ -5,11 +5,14 @@ tic;
 
 %Q2(a) Image Enhancement by Linear Contrasting
 church = imread('../data/church.png');
-church_linstretch = myLinearContrastStretching(church);
-imshow(church_linstretch);
+% church_linstretch = myLinearContrastStretching(church);
+% figure(1);
+% imshow(church_linstretch);
 
 %Q2(b) Image Enhancement using Independent channel Histogram Equalisation
 HM = imread('../data/HMInputImage.png');
 HM_he = myHE(HM);
-imshow(HM_he);
+figure(2);
+subplot(2,1,1),imshow(HM);
+subplot(2,1,2),imshow(HM_he);
 toc;
