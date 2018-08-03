d01cd53a54c7aa9ce34f550a28a93b74ca8a9be2
SConsul <sarthakconsul@gmail.com>
2018-08-03 21:38:54 +0530

    Removing copy

--- Assignment_1 - Copy/Q1/codes/myShrinkImageByFactorD.m
+++
@@ -1,5 +0,0 @@
-function output = myShrinkImageByFactorD(input, D)
-% Shrinks the input image by a factor of D
-    output = input([1:D:end],[1:D:end]);
-    
-end
--- Assignment_1 - Copy/Q2/codes/hm_test.m
+++
@@ -1,9 +0,0 @@
-clear all
-clc
-
-cdf_A = [1 2 3 4 6 7 8 10 11 19 20 50]; 
-img = [2 4 6 8 10 10; 11 14 13 20 18 70];
-[mem, mem] = ismember(img,cdf_A);
-
-img
-mem
