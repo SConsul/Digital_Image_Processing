58cd1e5f195e72cfc5f2fab7f8b926127c962043
Parthasarathi Khirwadkar <kparth98@gmail.com>
2018-08-03 18:04:53 +0530

    Adding myCLAHE2 and correction in myCLAHE

--- Assignment_1/Q2/codes/myCLAHE2.m
+++ Assignment_1/Q2/codes/myCLAHE2.m
@@ -1,12 +1,13 @@
-function [op]=myCLAHE(inp,N,thresh)
+function [op]=myCLAHE2(inp,N,thresh)
 %% Using nlfilter function
-    f = @(x) CLAHE_nlfilter(x,thresh);
+    f = @(x) CLAHE2_nlfilter(x,thresh);
     op = nlfilter(inp,[N N], f);
 end
 
-function op=CLAHE_nlfilter(A,thresh)
+function op=CLAHE2_nlfilter(A,thresh)
     edges = linspace(1,256,256);
     hist = histcounts(A,edges);
+    hist = hist/sum(hist);
     hist = clip(hist, thresh);
     cdf = cumsum(hist)/sum(hist);
     center = uint8(floor(size(A,1)*0.5) + 1);
