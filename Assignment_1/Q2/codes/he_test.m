1bd9f029c538382be1d5d245dec967bfb44b4457
SConsul <sarthakconsul@gmail.com>
2018-08-05 16:54:40 +0530

    Complete, but needs finetuning

---
+++ Assignment_1/Q2/codes/he_test.m
@@ -0,0 +1,56 @@
+function output = myHE(input,arg, varargin)
+% Image Enhancement using Histogram Equalisation (independent channel)
+    if(nargin==2) 
+        s = size(input);
+        mask = true(s(1), s(2));
+    else
+        mask = cell2mat(varargin(1,1));
+    end
+    
+    output = zeros(size(input));
+    
+    if(arg=='g')
+        cdf = CDF_gen(input(mask));
+        output = cell2mat(arrayfun(@(z)CDF(cdf,z),input,'UniformOutput',false));
+        output = output.*mask;
+    else
+          r_i = input(:,:,1);
+          g_i = input(:,:,2);
+          b_i = input(:,:,3);
+
+          cdf_r = CDF_gen(r_i(mask));
+          cdf_g = CDF_gen(g_i(mask));
+          cdf_b = CDF_gen(b_i(mask));
+
+          if(arg =='ind')
+              r_he = cell2mat(arrayfun(@(z)CDF(cdf_r,z),r_i,'UniformOutput',false));
+              g_he = cell2mat(arrayfun(@(z)CDF(cdf_g,z),g_i,'UniformOutput',false));
+              b_he = cell2mat(arrayfun(@(z)CDF(cdf_b,z),b_i,'UniformOutput',false));
+              
+          elseif(arg =='avg')
+              cdf_avg = (cdf_r + cdf_g + cdf_b)/3;
+        %     cdf_w = 0.299*cdf_r + 0.587*cdf_g + 0.114*cdf_b; 
+              r_he = cell2mat(arrayfun(@(z)CDF(cdf_avg,z),r_i,'UniformOutput',false));
+              g_he = cell2mat(arrayfun(@(z)CDF(cdf_avg,z),g_i,'UniformOutput',false));
+              b_he = cell2mat(arrayfun(@(z)CDF(cdf_avg,z),b_i,'UniformOutput',false));
+          end
+          
+          output(:,:,1) = r_he.*mask;
+          output(:,:,2) = g_he.*mask;
+          output(:,:,3) = b_he.*mask;
+%           output = cat(3, r_he.*mask, g_he.*mask, b_he.*mask);
+    end
+       
+end
+
+    function cdf = CDF_gen(ip)
+%         hist= imhist(ip);
+        hist = histcounts(ip,linspace(0,256,257),'Normalization','probability');
+%         cdf = double(cumsum(hist))/double(sum(hist));
+        cdf = cumsum(hist);
+    end
+
+    function op = CDF(cdf,ip)
+      op = double(255*cdf(floor(ip)+1));
+        
+    end
--- Assignment_1/Q2/codes/myCLAHE.m
+++ Assignment_1/Q2/codes/myCLAHE.m
@@ -1,52 +1,34 @@
+
 function output = myCLAHE(input,N,thresh)
-       offset = floor(N/2);
-       padded = padarray(int16(input),[offset offset 0], -1);
-       output = zeros(size(input));
+    offset = floor(N/2);
+    padded = padarray(int16(input),[offset offset 0], -1);
+    output = zeros(size(input));
 
     for iter = 1: size(input,3)
     pad = padded(:,:,iter);
        for j=1:size(input,2)
-           hist = get_hist(pad,offset+1,j+offset,offset,offset);
+           hist = histcounts(pad(1:N,j:min(j+N-1,size(pad,2))),0:1:256);
            for i=1:size(input,1)
-               output(i,j,iter) = applyHE(hist,pad(i+offset,j+offset),thresh);
-               hist_prev = get_hist(pad,i,j+offset,0,offset);
-               hist_next = get_hist(pad,min(i+N,size(pad,1)),j+offset,0,offset);
-               hist = hist - hist_prev + hist_next;
+               norm_hist = hist/sum(hist);
+               output(i,j,iter) = applyCLAHE(norm_hist,pad(i+offset,j+offset),thresh);
+               hist_prev = histcounts(pad(i,j:min(j+N-1,size(pad,2))),0:1:256);
+               hist_next = histcounts(pad(min(i+N,size(pad,1)),j:min(j+N-1,size(pad,2))),0:1:256);
+               hist = hist - hist_prev;
+               hist = hist + hist_next;
            end
        end 
     end
 end
 
-% function ouput=myAHE_vect(input,N)
-%    offset = floor(N/2);
-%    padded = padarray(int16(input),[offset offset], -1);
-%    output = zeros(size(input));
-% %    for j=1:size(input,2)
-%     hist_init = arrayfun(@(z)get_hist(padded,offset+1,z+offset,offset,offset);
-% %        for i=1:size(input,1)
-% %            output(i,j) = applyHE(hist,padded(i+offset,j+offset));
-% %            hist_prev = get_hist(padded,i,j+offset,0,offset);
-% %            hist_next = get_hist(padded,min(i+N,size(padded,1)),j+offset,0,offset);
-% %            hist = hist - hist_prev + hist_next;
-% %       end
-% %    end  
-% end
-
-function op = applyHE(hist,pixel_value,thresh)
-    hist = hist/sum(hist);
+function op = applyCLAHE(hist,pixel_value,thresh)
     hist = clip(hist, thresh);
     cdf = cumsum(hist);
     op = (cdf(pixel_value+1)*255);
 end
 
-function [hist]=get_hist(inp,x,y,x_offset,y_offset)
-    inp = int16(inp);
-    hist = histcounts(inp(x-x_offset:x+x_offset,y-y_offset:y+y_offset),linspace(0,256,257));
-end
-
 function clipped_hist = clip(hist, thresh)
     over = hist > thresh;
-    excess = sum(hist(over)) - sum(over)*thresh;
+    excess = sum(hist(over)-thresh);
     hist(over) = thresh;
     clipped_hist = hist + (excess/size(hist,2));
 end
