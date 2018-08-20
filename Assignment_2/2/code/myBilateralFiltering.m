2563ffa92baa92d2afcd3086d06d8a580701736f
Bhishma <bhishma.dedhia@gmail.com>
2018-08-20 23:47:38 +0530

    Tuning done, Pics added, Report added

--- Assignment_2/2/code/myBilateralFiltering.m
+++ Assignment_2/2/code/myBilateralFiltering.m
@@ -2,31 +2,18 @@
 %%Code for Bilateral filtering
     %Creating a gaussian weight in space common for all pixel windows
     hsize = floor(4*sigma_space);
-    [row_coord,column_coord] = meshgrid(-hsize:hsize,-hsize:hsize);
-    k1= sqrt(2*pi)*sigma_int;
-    k2= sqrt(2*pi)*sigma_space;
-    %gaussian_space = exp(-(row_coord.^2 + column_coord.^2)/(2*sigma_space^2))/k2;
+    c1= sqrt(2*pi)*sigma_int;
     gaussian_space = fspecial('gaussian',[2*hsize+1,2*hsize+1],sigma_space);
     output =zeros(size(input));
     [height,width] = size(input);
-    input1 =  padarray(input, [hsize hsize], inf);
+    input1 =  padarray(input, [hsize hsize], inf);%Infinite to avoid interference in intensity weights due to padding
     for i = 1+hsize:width+hsize
         for j = 1+hsize:height+hsize
-            %Constraints on the window coordinates
-%             window_xmin = max(1,i-hsize);
-%             window_xmax = min(width,i+hsize);
-%             window_ymin = max(1,j-hsize);
-%             window_ymax = min(height, j + hsize);
-            %Selecting the final window around pixel
-%            window = input(window_xmin:window_xmax, window_ymin:window_ymax);
             window = input1( i-hsize:i+hsize, j-hsize:j+hsize);
-            
             %Finding the intensity gaussian weights
-            gaussian_int = exp(-(input1(i,j)- window).^2/(2*sigma_int^2))/k1;
-            %Adjusting the size of gaussian_space kernel
-            %gaussian_space_adj = gaussian_space(hsize+1-(i-window_xmin):hsize+1+(window_xmax-i),hsize+1-(j-window_ymin):hsize+1+(window_ymax-j));
+            gaussian_int = exp(-(input1(i,j)- window).^2/(2*sigma_int^2))/c1;
             %calculating output pixel value
-            window(window == inf) = 0;
+            window(window == inf) = 0;%Setting padding to 0 while accounting for weights
             numerator = sum(gaussian_int.*gaussian_space.*window);
             denominator = sum(gaussian_int.*gaussian_space);
             output(i-hsize,j-hsize) = numerator/denominator;
--- Assignment_2/2/code/myMainScript.m
+++ Assignment_2/2/code/myMainScript.m
@@ -1,5 +1,5 @@
-%% MyMainScript
-
+%MyMainScript
+rng(42);
 tic;
 %% Processing image 1
 
@@ -11,156 +11,151 @@ noise = randn(size(input));
 input_noise = input + 0.05*(max(max(input)) - min(min(input)))*noise;
 input_noise = input_noise/(max(max(input_noise)) - min(min(input_noise)));
 
-output = myBilateralFiltering(input_noise,0.81,0.63);
+output = myBilateralFiltering(input_noise,1.178,0.1720);
 output = output/(max(max(output)) - min(min(output)));
 input = input/(max(max(input)) - min(min(input)));
 
 
 RMSD = sqrt(sum(sum((output-input).^2))/(width*height));
-%Display mask used
-hsize = floor(4*0.81);
-% [row_coord,column_coord] = meshgrid(-hsize:hsize,-hsize:hsize);
-% gaussian_space = exp(-(row_coord.^2 + column_coord.^2)/(2*(1.5).^2));
-gaussian_space = fspecial('gaussian',[2*hsize+1,2*hsize+1],0.81);
-figure(1)
-imshow(mat2gray(gaussian_space)),colorbar;
-title('Gaussian space mask used');
 
-%Display images
-numberColours = 200;
-colorScale = [[0:1/(numberColours-1):1]',[0:1/(numberColours-1):1]',[0:1/(numberColours-1):1]'];
-colormap(colorScale);
-daspect([1,1,1]);
-axis tight;
-figure(2);
-subplot(1,3,1);
-imshow(mat2gray(input)),colorbar;
-title('Input image 1');
-subplot(1,3,2);
-imshow(mat2gray(input_noise)),colorbar;
-title('Input image 1 (corrupted)'); 
-subplot(1,3,3);
-imshow(mat2gray(output)),colorbar;
-title('Output image 1 (Filtered)'); 
+hsize = floor(4*1.178);
+gaussian_space = fspecial('gaussian',[2*hsize+1,2*hsize+1],1.178);
+%% Displaying mask used
+my_imshow(mat2gray(gaussian_space),'Gaussian space mask used for Image 1')
+
+
+%% Displaying images
+my_imshow(input, "Original Image 1",input_noise,"Corrupted image 1",output,"Filtered image 1");
+%% Optimal sigma_space is : 1.178 and Optimal sigma_intensity is: 0.1720, where we get optimal RSMD to be=
 disp(RMSD);
+%% Calculating non optimal RMSD
+output1 = myBilateralFiltering(input_noise,1.178*0.9,0.1720);
+output1 = output1/(max(max(output1)) - min(min(output1)));
+output2 = myBilateralFiltering(input_noise,1.178*1.1,0.1720);
+output2 = output1/(max(max(output2)) - min(min(output2)));
+output3 = myBilateralFiltering(input_noise,1.178,0.1720*0.9);
+output3 = output3/(max(max(output3)) - min(min(output3)));
+output4 = myBilateralFiltering(input_noise,1.178,0.1720*1.1);
+output4 = output4/(max(max(output4)) - min(min(output4)));
 
-  
-%Computing non optimal RMSD
-sigma_space =  [0.9,1.1,1,1];
-sigma_int = [1,1,0.9,1.1];
-for i =1:4
-     output = myBilateralFiltering(input_noise,0.81*sigma_space(i),0.63*sigma_int(i));
-     output = output/(max(max(output)) - min(min(output)));
-     RMSD = sqrt(1/(width*height)*sum(sum((output-input).^2))); 
-     disp(RMSD);
+RMSD1 = sqrt(1/(width*height)*sum(sum((output1-input).^2))); 
+RMSD2 = sqrt(1/(width*height)*sum(sum((output2-input).^2))); 
+RMSD3 = sqrt(1/(width*height)*sum(sum((output3-input).^2))); 
+RMSD4 = sqrt(1/(width*height)*sum(sum((output4-input).^2))); 
 
-end
+%% RMSD with 0.9*sigma_space and 1*sigma_intensity
+disp(RMSD1);
+%% RMSD with 1.1*sigma_space and 1*sigma_intensity
+disp(RMSD2);
+%% RMSD with 1*sigma_space and 0.9*sigma_intensity
+disp(RMSD3);
+%% RMSD with 1*sigma_space and 1.1*sigma_intensity
+disp(RMSD4)
+
 toc;
 
-tic;
+
 %% Processing image 2
-storedStructure = load('../data/honeyCombReal_Noisy.mat');
-input = storedStructure.imgCorrupt;
+rng(42)
+tic;
+input=im2double(imread('../data/honeyCombReal.png'));
 [height,width] = size(input);
 
 noise = randn(size(input));
 input_noise = input + 0.05*(max(max(input)) - min(min(input)))*noise;
 input_noise = input_noise/(max(max(input_noise)) - min(min(input_noise)));
 
-output = myBilateralFiltering(input_noise,0.62,1.1);
+output = myBilateralFiltering(input_noise,0.7107,0.7503);
 output = output/(max(max(output)) - min(min(output)));
 input = input/(max(max(input)) - min(min(input)));
 
 
 RMSD = sqrt(sum(sum((output-input).^2))/(width*height));
-%Display mask used
-hsize = floor(4*0.62);
-gaussian_space = fspecial('gaussian',[2*hsize+1,2*hsize+1],0.62);
-figure(1)
-imshow(mat2gray(gaussian_space)),colorbar;
-title('Gaussian space mask used');
+hsize = floor(4*0.7107);
+gaussian_space = fspecial('gaussian',[2*hsize+1,2*hsize+1],0.7107);
+%% Displaying mask used
+my_imshow(mat2gray(gaussian_space),'Gaussian space mask used for Image 2')
 
-%Display images
-numberColours = 200;
-colorScale = [[0:1/(numberColours-1):1]',[0:1/(numberColours-1):1]',[0:1/(numberColours-1):1]'];
-colormap(colorScale);
-daspect([1,1,1]);
-axis tight;
-figure(2);
-subplot(1,3,1);
-imshow(mat2gray(input)),colorbar;
-title('Input image 1');
-subplot(1,3,2);
-imshow(mat2gray(input_noise)),colorbar;
-title('Input image 1 (corrupted)'); 
-subplot(1,3,3);
-imshow(mat2gray(output)),colorbar;
-title('Output image 1 (Filtered)'); 
+
+%% Displaying images
+my_imshow(input, "Original Image 2",input_noise,"Corrupted image 2",output,"Filtered image 2"); 
+%% Optimal sigma_space is : 0.7107 and Optimal sigma_intensity is: 0.7503 where we get optimal RSMD to be=
 disp(RMSD);
 
-  
-%Computing non optimal RMSD
-sigma_space =  [0.9,1.1,1,1];
-sigma_int = [1,1,0.9,1.1];
-for i =1:4
-     output = myBilateralFiltering(input_noise,0.62*sigma_space(i),1.1*sigma_int(i));
-     output = output/(max(max(output)) - min(min(output)));
-     RMSD = sqrt(1/(width*height)*sum(sum((output-input).^2))); 
-     disp(RMSD);
+%% Calculating non optimal RMSD  
+output1 = myBilateralFiltering(input_noise,0.7107*0.9,0.7503);
+output1 = output1/(max(max(output1)) - min(min(output1)));
+output2 = myBilateralFiltering(input_noise,0.7107*1.1,0.7503);
+output2 = output1/(max(max(output2)) - min(min(output2)));
+output3 = myBilateralFiltering(input_noise,0.7107,0.7503*0.9);
+output3 = output3/(max(max(output3)) - min(min(output3)));
+output4 = myBilateralFiltering(input_noise,0.7107,0.7503*1.1);
+output4 = output4/(max(max(output4)) - min(min(output4)));
 
-end
+RMSD1 = sqrt(1/(width*height)*sum(sum((output1-input).^2))); 
+RMSD2 = sqrt(1/(width*height)*sum(sum((output2-input).^2))); 
+RMSD3 = sqrt(1/(width*height)*sum(sum((output3-input).^2))); 
+RMSD4 = sqrt(1/(width*height)*sum(sum((output4-input).^2))); 
+
+%% RMSD with 0.9*sigma_space and 1*sigma_intensity
+disp(RMSD1);
+%% RMSD with 1.1*sigma_space and 1*sigma_intensity
+disp(RMSD2);
+%% RMSD with 1*sigma_space and 0.9*sigma_intensity
+disp(RMSD3);
+%% RMSD with 1*sigma_space and 1.1*sigma_intensity
+disp(RMSD4)
 toc;
 
-tic;
+
 %% Processing image 3
-storedStructure = load('../data/grassNoisy.mat');
-input = storedStructure.imgCorrupt;
+rng(42)
+tic;
+input=im2double(imread('../data/grass.png'));
 [height,width] = size(input);
 
 noise = randn(size(input));
 input_noise = input + 0.05*(max(max(input)) - min(min(input)))*noise;
 input_noise = input_noise/(max(max(input_noise)) - min(min(input_noise)));
 
-output = myBilateralFiltering(input_noise,0.79,0.9);
+output = myBilateralFiltering(input_noise,0.693,0.86);
 output = output/(max(max(output)) - min(min(output)));
 input = input/(max(max(input)) - min(min(input)));
 
 
 RMSD = sqrt(sum(sum((output-input).^2))/(width*height));
-%Display mask used
-hsize = floor(4*0.62);
-gaussian_space = fspecial('gaussian',[2*hsize+1,2*hsize+1],0.62);
-figure(1)
-imshow(mat2gray(gaussian_space)),colorbar;
-title('Gaussian space mask used');
+hsize = floor(4*0.693);
+gaussian_space = fspecial('gaussian',[2*hsize+1,2*hsize+1],0.693);
+%% Displaying mask used
+my_imshow(mat2gray(gaussian_space),'Gaussian space mask used for Image 3')
 
-%Display images
-numberColours = 200;
-colorScale = [[0:1/(numberColours-1):1]',[0:1/(numberColours-1):1]',[0:1/(numberColours-1):1]'];
-colormap(colorScale);
-daspect([1,1,1]);
-axis tight;
-figure(2);
-subplot(1,3,1);
-imshow(mat2gray(input)),colorbar;
-title('Input image 1');
-subplot(1,3,2);
-imshow(mat2gray(input_noise)),colorbar;
-title('Input image 1 (corrupted)'); 
-subplot(1,3,3);
-imshow(mat2gray(output)),colorbar;
-title('Output image 1 (Filtered)'); 
+
+%% Displaying images
+my_imshow(input, "Original Image 3",input_noise,"Corrupted image 3",output,"Filtered image 3");
+%% Optimal sigma_space is : 0.693 and Optimal sigma_intensity is: 0.86, where we get optimal RSMD to be=
 disp(RMSD);
 
-  
-%Computing non optimal RMSD
-sigma_space =  [0.9,1.1,1,1];
-sigma_int = [1,1,0.9,1.1];
-for i =1:4
-     output = myBilateralFiltering(input_noise,0.9*sigma_space(i),0.79*sigma_int(i));
-     output = output/(max(max(output)) - min(min(output)));
-     RMSD = sqrt(1/(width*height)*sum(sum((output-input).^2))); 
-     disp(RMSD);
+%% Calculating non optimal RMSD  
+output1 = myBilateralFiltering(input_noise,0.693*0.9,0.86);
+output1 = output1/(max(max(output1)) - min(min(output1)));
+output2 = myBilateralFiltering(input_noise,0.693*1.1,0.86);
+output2 = output1/(max(max(output2)) - min(min(output2)));
+output3 = myBilateralFiltering(input_noise,0.693,0.86*0.9);
+output3 = output3/(max(max(output3)) - min(min(output3)));
+output4 = myBilateralFiltering(input_noise,0.693,0.86*1.1);
+output4 = output4/(max(max(output4)) - min(min(output4)));
 
-end
+RMSD1 = sqrt(1/(width*height)*sum(sum((output1-input).^2))); 
+RMSD2 = sqrt(1/(width*height)*sum(sum((output2-input).^2))); 
+RMSD3 = sqrt(1/(width*height)*sum(sum((output3-input).^2))); 
+RMSD4 = sqrt(1/(width*height)*sum(sum((output4-input).^2))); 
+
+%% RMSD with 0.9*sigma_space and 1*sigma_intensity
+disp(RMSD1);
+%% RMSD with 1.1*sigma_space and 1*sigma_intensity
+disp(RMSD2);
+%% RMSD with 1*sigma_space and 0.9*sigma_intensity
+disp(RMSD3);
+%% RMSD with 1*sigma_space and 1.1*sigma_intensity
+disp(RMSD4)
 toc;
