9bf8d0621c2f5ff48b6843e6679c210de0a1cd18
SConsul <sarthakconsul@gmail.com>
2018-10-15 21:16:01 +0530

    mySVD complete

--- Assignment_4/assignment4_SVD_FaceRecognition/4/code/MyMainScript.m
+++ Assignment_4/assignment4_SVD_FaceRecognition/4/code/MyMainScript.m
@@ -2,16 +2,19 @@
 square = magic(9);
 [u s v] = MySVD(square);
 err_sq = square - u*s*v';
-err_sq = sum(sum(err_sq.*err_sq))
+err_sq = sum(sum(err_sq.*err_sq))/numel(square);
+disp(['Reconstruction Error for a square matrix = ', num2str(err_sq)]);
 
 tall = rand(12,9);
 [u s v] = MySVD(tall);
 err_tall = tall - u*s*v';
-err_tall = sum(sum(err_tall.*err_tall))
+err_tall = sum(sum(err_tall.*err_tall))/numel(tall);
+disp(['Reconstruction Error for a tall matrix = ', num2str(err_tall)]);
 
 fat = rand(6,13);
 [u s v] = MySVD(fat);
 err_fat = fat - u*s*v';
-err_fat = sum(sum(err_fat.*err_fat))
+err_fat = sum(sum(err_fat.*err_fat))/numel(fat);
+disp(['Reconstruction Error for a fat matrix = ', num2str(err_fat)]);
 
-%as seen, all variants of input matrix A result in small errors (Frobenius norm of difference) betwwen original and recontructied imagesin 
+%as seen, all variants of input matrix A result in small errors average squared error ( = Frobenius norm of difference/numel) between original and recontructied image
