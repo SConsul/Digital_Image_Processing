c6ce7f277074b9c53a7309326400122fa7365895
SConsul <sarthakconsul@gmail.com>
2018-10-28 19:30:15 +0530

    Q4 added

--- Assignment_5/assignment5_DFT/3/code/myMainScript.m
+++ Assignment_5/assignment5_DFT/3/code/myMainScript.m
@@ -1,5 +1,7 @@
 %% MyMainScript
+clear all
 close all
+clc
 tic;
 %% Your code here
 
@@ -8,7 +10,7 @@ img = img.Z;
 FT = fftshift(fft2(img));
 FT_plot = log(1 + abs(FT));
 figure;
-imshow(FT_plot, [min(min(FT_plot)), max(max(FT_plot))]); colormap jet ; 
+imagesc(FT_plot, [min(min(FT_plot)), max(max(FT_plot))]); colormap gray ; colorbar;
 title('Log Fourier Transform of original image', 'Fontsize', 12, 'Fontname', 'Cambria');
 % The input is a barbara image with (top-right to bottom left) diagonal lines of noise added. This
 % implies that the noise adds points to the FFT such that they would be
@@ -21,16 +23,16 @@ filteredFT = FT.*notch_filter;
 filteredFT_plot = log(1+abs(filteredFT));
 
 figure;
-imshow(notch_filter, [min(min(notch_filter)), max(max(notch_filter))]); colormap jet ; 
+imagesc(notch_filter, [min(min(notch_filter)), max(max(notch_filter))]); colormap jet ; colorbar;
 
 figure;
-imshow(filteredFT_plot, [min(min(filteredFT_plot)), max(max(filteredFT_plot))]); colormap jet ; 
+imagesc(filteredFT_plot, [min(min(filteredFT_plot)), max(max(filteredFT_plot))]); colormap jet ; colorbar;
 title('Log Fourier Transform of filtered image', 'Fontsize', 12, 'Fontname', 'Cambria');
 filteredimage = ifft2(ifftshift(filteredFT));
 figure;
-imshow(filteredimage, [min(min(filteredimage)), max(max(filteredimage))]);
+imshow(filteredimage, [min(min(filteredimage)), max(max(filteredimage))]);colorbar;
 title('Filtered image', 'Fontsize', 12, 'Fontname', 'Cambria');
 figure;
-imshow(img, [min(min(img)), max(max(img))]);
+imshow(img, [min(min(img)), max(max(img))]);colorbar;
 title('Original image', 'Fontsize', 12, 'Fontname', 'Cambria');
 toc;
---
+++ Assignment_5/assignment5_DFT/4/code/iLPF.m
@@ -0,0 +1,12 @@
+function idealLPF = iLPF(D, m , n)
+    c_x = (m+1)/2;
+    c_y = (n+1)/2;
+    idealLPF = zeros(m,n);
+    for u = 0:n/2
+        for v = 0:n/2
+            if(u^2+v^2 < D^2)
+                idealLPF( floor(c_x - u):ceil(c_x + u), floor(c_y - v) : ceil(c_x + v)) = 1;
+            end
+        end
+    end     
+end
