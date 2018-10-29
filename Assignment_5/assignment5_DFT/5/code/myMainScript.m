26c7fd7963d182233c5411ca6d6b5d4fdeeab668
SConsul <sarthakconsul@gmail.com>
2018-10-29 17:07:02 +0530

    Updated Q5

--- Assignment_5/assignment5_DFT/5/code/myMainScript.m
+++ Assignment_5/assignment5_DFT/5/code/myMainScript.m
@@ -3,52 +3,64 @@ close all
 clear all
 clc
 
-im = im2double(imread('../data/barbara256.png'))*255;
+im = double(imread('../data/barbara256.png'));
 
 %%  adding gaussian noise
 sigma = 20;
-im1_gaussian = im+rand(size(im)).*sigma;
+im1_gaussian = im+randn(size(im)).*sigma;
 
 %% Part (A)
 tic;
 im2_a = myPCADenoising1(im1_gaussian,[7 7],sigma);
-rmse = norm((im2_a-im),2)/norm(im,2);
+rmse = norm((im2_a-im),'fro')/norm(im,'fro');
+disp('RMSE in Part A:');
+disp(rmse);
 figure;
 imshow(im/255);
-title('Original image', 'Fontsize', 12, 'Fontname', 'Cambria');
-
+title('Original image', 'Fontsize', 12, 'Fontname', 'Cambria'); 
 figure;
-imshow(im1_gaussian/255);
-title('Noisy image', 'Fontsize', 12, 'Fontname', 'Cambria');
-
-figure;
-imshow(im2_a/255);
-title('Denoised Image (part a)', 'Fontsize', 12, 'Fontname', 'Cambria');
+imshowpair(im1_gaussian/255, im2_a/255, 'montage');
+title('Noisy image and Denoised Image using all patches', 'Fontsize', 12, 'Fontname', 'Cambria'); 
 
 %% Part (B)
+im2_b = myPCADenoising2(im1_gaussian,[7 7], [31,31], sigma, 200);
+rmse = norm((im2_b-im),'fro')/norm(im,'fro');
+disp('RMSE in Part B:');
+disp(rmse);
+figure;
+imshowpair(im1_gaussian/255, im2_b/255, 'montage');
+title('Noisy image and Denoised Image using 200 most similar patches', 'Fontsize', 12, 'Fontname', 'Cambria'); 
 
 %% Part (C)
-
+im2_c = myBilateralFiltering(im1_gaussian,5,sigma);
+rmse = norm((im2_c-im),'fro')/norm(im,'fro');
+disp('RMSE using Bilateral Filtering is:');
+disp(rmse);
+figure;
+imshowpair(im1_gaussian/255, im2_c/255, 'montage');
+title('Noisy image and Denoised Image (Part C)', 'Fontsize', 12, 'Fontname', 'Cambria'); 
 %% Part (D)
 
 % Image Acquisition with a sufficient exposure time
-im1_poisson = poissonrnd(im);
+im1_poisson = poissrnd(im);
+im2_poisson = myPCADenoising2(sqrt(im1_poisson), [7,7], [31 31], 0.25, 200);
+im2_poisson = im2_poisson.^2;
+rmse = norm((im2_poisson-im1_poisson),'fro')/norm(im,'fro');
+disp('RMSE for sufficient exposure time is:');
+disp(rmse);
 figure;
-imshow(im1_poisson/255.0); colorbar;
-title('Image with Poisson Noise', 'Fontsize', 12, 'Fontname', 'Cambria');
-im2_poisson = myPCADenoising1(sqrt(im1_poisson), [7,7], [31 31], 0.25);
-figure;
-imshow(im2_poisson.*im2_poisson/255.0); colorbar;
-title('Denoised Image with Poisson Noise', 'Fontsize', 12, 'Fontname', 'Cambria');
+imshowpair(im1_poisson/255.0, im2_poisson/255.0, 'montage');
+title('Noisy and Denoised Image with sufficient exposure time', 'Fontsize', 12, 'Fontname', 'Cambria');
 
 % Image Acquisition with a lower exposure time
-im1_poisson_le = poissonrnd(im/20);
-figure;
-imshow(im1_poisson_le/255.0); colorbar;
-title('Image with Poisson Noise, with low exposure', 'Fontsize', 12, 'Fontname', 'Cambria');
-im2_poisson_le = myPCADenoising1(sqrt(im1_poisson_le), [7,7], [31 31], 0.25);
-figure;
-imshow(im2_poisson_le.*im2_poisson_le/255.0); colorbar;
-title('Denoised Image with Poisson Noise', 'Fontsize', 12, 'Fontname', 'Cambria');
+im1_poisson_le = poissrnd(im/20);
+im2_poisson_le = myPCADenoising2(sqrt(im1_poisson_le), [7,7], [31 31], 0.25, 200);
+im2_poisson_le = im2_poisson_le.^2;
+rmse = norm((im2_poisson_le-im1_poisson),'fro')/norm(im,'fro');
+disp('RMSE for low exposure time is:');
+disp(rmse);
+figure; 
+imshowpair(im1_poisson_le/255.0, im2_poisson_le/255.0, 'montage');
+title('Noisy and Denoised Image with low exposure', 'Fontsize', 12, 'Fontname', 'Cambria');
 
 toc;
